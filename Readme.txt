41e06ff78a1c4554e15741fec770d66f83f1721e
Arif Reza <arifreza_rifat@yahoo.com>
2019-08-05 23:07:22 +0600

    UCML Interpreter and Runtime

--- Readme.txt
+++
@@ -1,6 +0,0 @@
-Submitted By:-
-
-16701053 - Md. Arif Reza
-16701032 - Angel Sharma
-16701076 - Rezaul Karim
-
--- TestRunner.java
+++ TestRunner.java
@@ -10,52 +10,37 @@ class TestRunner {
 
         for (int i = 0; i < listOfFiles.length; i++) {
             if (listOfFiles[i].isFile()) {
-                System.out.println(">> Running: uc test\\" + listOfFiles[i].getName());
-            } else if (listOfFiles[i].isDirectory()) {
-                // System.out.println("Directory " + listOfFiles[i].getName());
-            }
+                System.out.println(">> Running: ./uc test/" + listOfFiles[i].getName());
 
-            try {
-                // -- Linux --
-                
-                // Run a shell command
-                // Process process = Runtime.getRuntime().exec("ls /home/mkyong/");
-        
-                // Run a shell script
-                // Process process = Runtime.getRuntime().exec("path/to/hello.sh");
-        
-                // -- Windows --
-                
-                // Run a command
-                //Process process = Runtime.getRuntime().exec("cmd /c dir C:\\Users\\mkyong");
-        
-                //Run a bat file
-                Process process = Runtime.getRuntime().exec(
-                        "uc test\\"+listOfFiles[i].getName(), null, new File(System.getProperty("user.dir")));
-        
-                StringBuilder output = new StringBuilder();
-        
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(process.getInputStream()));
-        
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    output.append(line + "\n");
+                try {
+                    Process process;
+                    process = Runtime.getRuntime().exec(
+                        "./build/uc test/"+listOfFiles[i].getName(), null, new File(System.getProperty("user.dir")));
+            
+                    StringBuilder output = new StringBuilder();
+            
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(process.getInputStream()));
+            
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        output.append(line + "\n");
+                    }
+            
+                    int exitVal = process.waitFor();
+                    if (exitVal == 0) {
+                        System.out.println("<< Success!");
+                        System.out.println(output);
+                        // System.exit(0);
+                    } else {
+                        //abnormal...
+                    }
+            
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-        
-                int exitVal = process.waitFor();
-                if (exitVal == 0) {
-                    System.out.println("<< Success!");
-                    System.out.println(output);
-                    // System.exit(0);
-                } else {
-                    //abnormal...
-                }
-        
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
         }
     }
